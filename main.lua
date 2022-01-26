b5f851fd7d5dd29d705a6ca165a0b48f163a2eec
fran1215 <66244249+fran1215@users.noreply.github.com>
2022-01-26 11:30:46 +0100

    check if word on list

--- main.lua
+++ main.lua
@@ -6,3 +6,6 @@ local worldlet_word_table = Read_file("word-list.txt")
 if worldlet_word_table == nil then os.exit(-1) end
 
 local random_word = Pick_random_word(worldlet_word_table)
+local input = read_terminal(worldlet_word_table)
+
+print(input)
--- terminal.lua
+++ terminal.lua
@@ -1,17 +1,19 @@
+require "parse-words"
+
 local escape = {
     ["red"] = "\027[31m",
     ["green"] = "\027[32m",
-    ["orange"] = "\027[33m",
+    ["yellow"] = "\027[33m",
     ["strikethrough"] = "\027[9m",
     ["bold"] = "\027[1m",
 }
 
 function read_terminal(list)
-    io.write("Input a word of length 5:\n")
+    io.write("Input a valid word of length 5:\n")
     io.flush()
     input = io.read()
-    while string.len(input) ~= 5 do
-        io.write("Input a word of length 5:\n")
+    while (string.len(input) ~= 5) or (not On_list(input, list)) do
+        io.write("Input a valid word of length 5:\n")
         io.flush()
         input = io.read()
     end
@@ -20,8 +22,7 @@ function read_terminal(list)
 end
 
 function write_terminal(msg)
-
-    print(escape["green"] .. msg)
+    print(escape["orange"] .. escape["bold"] .. msg)
 end
 
-write_terminal("Hola")
+--write_terminal("Hola")
