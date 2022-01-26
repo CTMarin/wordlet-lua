624d1c5b95095da7a07aa9eeca1bbe3fbaf8a06c
CTMarin <carlomaco11@gmail.com>
2022-01-26 10:38:15 +0100

    fix: git moment

--- main.lua
+++ main.lua
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 require("parse-words")
-=======
 require "terminal"
->>>>>>> db53e39ed228a1a477629378678ef71d950eeef6
 
 local wordlet_word_file = read_file("word-list.txt")
 if wordlet_word_file == nil then os.exit(-1) end
