603ea46ad4a9699dbec01020fbc6d1d398eec622
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-01-26 10:38:07 +0100

    fix: iterate once on list

--- main.lua
+++ main.lua
@@ -1,11 +1,12 @@
 require "terminal"
 
 local function read_file(path)
-    local file = io.open(path, "rb") -- r read mode and b binary mode
-    if not file then return nil end
-    local content = file:read "*a" -- *a or *all reads the whole file
-    file:close()
-    return content
+    local result = {}
+    for line in io.lines(path)
+    do 
+        result[#result+1] = line
+    end
+    return result
 end
 
 local function string_to_table(input)
@@ -23,8 +24,7 @@ local function pick_rnd_word(word_table)
     return word_table[selected_word_index]
 end
 
-local wordlet_word_file = read_file("word-list.txt")
-if wordlet_word_file == nil then os.exit(-1) end
+local worldlet_word_table = read_file("word-list.txt")
+if worldlet_word_table == nil then os.exit(-1) end
 
-local worldlet_word_table = string_to_table(wordlet_word_file)
 print(pick_rnd_word(worldlet_word_table))
