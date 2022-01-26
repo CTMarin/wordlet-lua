4506500171e52d103ee26fd2e87f45ba053eedad
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-01-26 10:33:15 +0100

    new: file error handeling

--- main.lua
+++ main.lua
@@ -22,6 +22,7 @@ local function pick_rnd_word(word_table)
 end
 
 local wordlet_word_file = read_file("word-list.txt")
-local worldlet_word_table = string_to_table(wordlet_word_file)
+if wordlet_word_file == nil then os.exit(-1) end
 
+local worldlet_word_table = string_to_table(wordlet_word_file)
 print(pick_rnd_word(worldlet_word_table))
