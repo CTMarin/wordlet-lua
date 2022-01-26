be4f3919eaa5af415fcb6004e5d7ca60657e1640
CTMarin <carlomaco11@gmail.com>
2022-01-26 10:35:36 +0100

    feat: modularized words parse

--- main.lua
+++ main.lua
@@ -1,25 +1,4 @@
-local function read_file(path)
-    local file = io.open(path, "rb") -- r read mode and b binary mode
-    if not file then return nil end
-    local content = file:read "*a" -- *a or *all reads the whole file
-    file:close()
-    return content
-end
-
-local function string_to_table(input)
-    local list = {}
-    for str in string.gmatch(input, "([^"..'\n'.."]+)") do
-        table.insert(list, str)
-    end
-    return list
-end
-
-local function pick_rnd_word(word_table)
-    local table_size = #word_table
-    math.randomseed()
-    local selected_word_index = math.random(0, table_size)
-    return word_table[selected_word_index]
-end
+require("parse-words")
 
 local wordlet_word_file = read_file("word-list.txt")
 local worldlet_word_table = string_to_table(wordlet_word_file)
